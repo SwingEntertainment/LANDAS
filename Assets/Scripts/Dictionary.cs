24811c33d3aa00504668ad8d16efbf63bcd95036
ALeycano <leycano32@gmail.com>
2025-06-27 06:14:47 +0800

    Dictionary proto

--- Assets/Scripts/Dictionary.cs
+++ Assets/Scripts/Dictionary.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BackButton : MonoBehaviour
+public class Dictionary : MonoBehaviour
 {
     public void OnBackButtonPressed()
     {
