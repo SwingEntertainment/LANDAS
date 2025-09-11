45b2ff701b97c2bb52a0a481d3a4b6affb79b4df
RZregio <regioroland011@gmail.com>
2025-09-11 09:01:20 +0800

    LANDAS#11: Ticket: [DEV] Scene 1 Part 3- MainMenu by Roland

---
+++ Assets/Scripts/Menus/StorySequence/NextPageFunction.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryPagesFunction : MonoBehaviour
+{
+    [SerializeField] private GameObject[] storyPages;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private string nextScene = "GameMenu";
+
+    private int currentPage = 0;
+
+    private void Start()
+    {
+        ShowPage(0);
+    }
+
+    private void OnEnable()
+    {
+        currentPage = 0;
+        ShowPage(currentPage);
+    }
+
+    public void NextPage()
+    {
+        if (currentPage < storyPages.Length - 1)
+        {
+            currentPage++;
+            ShowPage(currentPage);
+        }
+        else
+        {
+            MainMenu.SetStoryPlayed();
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    private void ShowPage(int index)
+    {
+        for (int i = 0; i < storyPages.Length; i++)
+            storyPages[i].SetActive(i == index);
+
+        nextButton.gameObject.SetActive(true);
+    }
+}
